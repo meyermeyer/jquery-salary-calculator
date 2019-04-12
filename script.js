3fbfab7d389fbbf55ea767addc3085f00854a1f5
Meyer <ehlmeyer@gmail.com>
2019-04-12 15:55:42 -0500

    js mapped out

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function documentReady(){
 }
 
 
+//handle Submit button click
+//create employee array
+//create employee object from input vals
+//add object to array
+//display employee array on DOM
+
+//display monthly costs
+    //loop through employee array
+    //take annual salary from each employee object, divide by 12, and add
+    //display monthly total
+    //if total>20k, change background to red
+
+//handle delete button click
+//create employee object from input vals
+//remove object from array
+//display new employee array on DOM
+
+
